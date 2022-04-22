2a3f7dfb93291bde09e5dc26bdc189a144fcf8de
NooSound <al.kremnev@gmail.com>
2022-04-22 13:18:26 +0300

    5) добавлен автотест страниц с продуктом, проверка кнопки "добавить в корзину", проверка корректной цены, и успешного добавления продукта в корзину. помечен баг как xfail

--- Pages/locators.py
+++ Pages/locators.py
@@ -4,8 +4,14 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-
 class LoginPageLocators():
     LOGIN_URL = "login/"
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    ADDED_NAME_TO_BASKET = (By.XPATH, "//*[@id='messages']/div[1]/div/strong")
+    PRICE = (By.XPATH, "//*[@id='content_inner']/article/div[1]/div[2]/p[1]")
+    ADDED_PRICE = (By.XPATH, "//*[@id='messages']/div[3]/div/p[1]/strong")
+    PRODUCT_NAME = (By.XPATH, "//*[@id='content_inner']/article/div[1]/div[2]/h1")
---
+++ Pages/product_page.py
@@ -0,0 +1,30 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+
+
+class ProductPage(BasePage):
+
+    def add_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
+        button.click()
+        self.solve_quiz_and_get_code()
+
+
+
+    def should_be_add_to_basket_btn(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_BTN), "No button"
+
+
+    def should_be_successfylly_added(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        added_product_name = self.browser.find_element(*ProductPageLocators.ADDED_NAME_TO_BASKET).text
+        assert added_product_name == product_name, "No success added product"
+
+    def should_be_correct_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRICE)
+        added_product_price = self.browser.find_element(*ProductPageLocators.ADDED_PRICE)
+        assert product_price == added_product_price, 'incorrect price'
+
+
